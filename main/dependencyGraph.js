371d3dde69efafe3dc5b6a74bb5fe6c649d52f09
stefan <stefan.breitenstein@gmail.com>
2015-10-26 21:08:16 +0100

    remove cytoscape from backend

--- main/dependencyGraph.js
+++ main/dependencyGraph.js
@@ -29,22 +29,34 @@ define(['main/cytoscapeInstance'], function (cytoscape) {
 
     return {
         getGraphFor: function (data) {
+            var result = {};
+            result.nodes=[];
+            result.edges = [];
+
             data.forEach(function (element) {
 
                 var srcId = element.src.split("\\");
                 srcId = srcId[srcId.length - 1];
-
-                addNode(srcId);
+                if(!result.nodes.some(function(e){return e === srcId})){
+                    result.nodes.push(srcId);
+                }
 
                 element.dep.forEach(function (depElement) {
                     var depId = depElement.split("\\");
                     depId = depId[depId.length - 1];
-                    addNode(depId);
-                    addEdge(srcId,depId);
+                    if(!result.nodes.some(function(e){return e === depId})){
+                        result.nodes.push(depId);
+                    }
+                    if(!result.edges.some(function(e){return e.from === srcId && e.to===depId})){
+                        result.edges.push({
+                            from:srcId,
+                            to:depId
+                        });
+                    }
                 });
 
             });
-            return cytoscape.json();
+            return result;
         }
     };
 });

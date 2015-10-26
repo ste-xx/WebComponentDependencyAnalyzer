4757251600faf396bbeef9aabc32bbd000e3c6c1
stefan <stefan.breitenstein@gmail.com>
2015-10-26 21:43:32 +0100

    add unittests

--- test/dependencyGraph.js
+++ test/dependencyGraph.js
@@ -6,12 +6,7 @@ describe('dependencyGraph', function () {
     before(function() {
         requirejs.config({
             baseUrl: __dirname + '/..',
-            nodeRequire: require,
-            map: {
-                '*': {
-                    'main/cytoscapeInstance': 'test/mocks/cytoscapeInstanceMock'
-                }
-            }
+            nodeRequire: require
         });
     });
 
@@ -31,6 +26,65 @@ describe('dependencyGraph', function () {
                 dep:[
                     'dependent.html'
                 ]
+            }]);
+            expect(result.nodes.length).to.equal(2);
+            expect(result.nodes).to.contain('index.html','dependent.html');
+            expect(result.edges.length).to.equal(1);
+            expect(result.edges).to.contain({
+                from:'index.html',
+                to:'dependent.html'
+            });
+        });
+
+        it('should create only unique nodes', function () {
+            var fv = requirejs('main/dependencyGraph');
+            var result = fv.getGraphFor([{
+                src:'index.html',
+                dep:['dependent.html']
+            },{ src:'index.html',
+                dep:['other.html']
+            }]);
+            expect(result.nodes.length).to.equal(3);
+            expect(result.nodes).to.contain('index.html','dependent.html','other.html');
+            expect(result.edges.length).to.equal(2);
+            expect(result.edges).to.contain({
+                from:'index.html',
+                to:'dependent.html'
+            });
+            expect(result.edges).to.contain({
+                from:'index.html',
+                to:'other.html'
+            });
+        });
+
+        it('should create only unique nodes (circular dependency)', function () {
+            var fv = requirejs('main/dependencyGraph');
+            var result = fv.getGraphFor([{
+                src:'index.html',
+                dep:['dependent.html']
+            },{ src:'dependent.html',
+                dep:['index.html']
+            }]);
+            expect(result.nodes.length).to.equal(2);
+            expect(result.nodes).to.contain('index.html','dependent.html');
+            expect(result.edges.length).to.equal(2);
+            expect(result.edges).to.contain({
+                from:'index.html',
+                to:'dependent.html'
+            });
+            expect(result.edges).to.contain({
+                from:'dependent.html',
+                to:'index.html'
+            });
+        });
+
+        it('should create only unique edges', function () {
+            var fv = requirejs('main/dependencyGraph');
+            var result = fv.getGraphFor([{
+                src:'index.html',
+                dep:['dependent.html']
+            },{ src:'index.html',
+                dep:['dependent.html']
             }]);
             expect(result.nodes.length).to.equal(2);
             expect(result.nodes).to.contain('index.html','dependent.html');
